3373c43a47918b1a2a3cd387e1fa157fb229ae21
Diamsyah M Dida <didadiamsyah02@gmail.com>
2021-10-11 09:57:47 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,6 @@
 
 const Koa = require('koa')
 const koa = new Koa()
-// const convert = require('koa-convert')
-// const bodyParser = require('koa-bodyparser')
-// const cors = require('@koa/cors')
 const RouteV1 = require('./app/v1/routes')
 const middleware = require('./helpers/middleware')
 require('dotenv').config()
